8cc4e518650e0573e559c8e45287396b751a6a61
Tyom Semonov <tyom.semonov@tomoro.ai>
2025-09-16 23:05:07 +0100

    Release v0.1.1

--- Formula/kcm.rb
+++ Formula/kcm.rb
@@ -1,8 +1,8 @@
 class Kcm < Formula
   desc "Keychain Master - Secure secret management for macOS"
   homepage "https://github.com/tyom/kcm"
-  url "https://github.com/tyom/kcm/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/tyom/kcm/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "PLACEHOLDER"
   license "MIT"
 
   depends_on :macos
