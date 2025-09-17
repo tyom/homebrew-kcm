84fc54b0948a45d0d0bc2e40599cdb9c0e538a7a
Tyom Semonov <tyom.semonov@tomoro.ai>
2025-09-17 14:32:59 +0100

    Update formula SHA256 for v0.2.0

--- Formula/kcm.rb
+++ Formula/kcm.rb
@@ -2,7 +2,7 @@ class Kcm < Formula
   desc "Keychain Master - Secure secret management for macOS"
   homepage "https://github.com/tyom/kcm"
   url "https://github.com/tyom/kcm/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "15195b05c657dc312c0e24d2631317ebebbb8a15ba20009fe83fa76e0b91cc76"
   license "MIT"
 
   depends_on :macos
